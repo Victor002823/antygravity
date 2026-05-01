199cab9ddeb62009747ba9d4a83ee38b885e3897
victor002823 <naviltc28@gmail.com>
2026-05-01 15:48:45 -0600

    fix r2 image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-49eda2722f8e480d9a3801c4aa48e1d1.r2.dev',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
